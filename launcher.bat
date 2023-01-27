609ab976184f076f20a59375752810258e26e69d
mariusjenin <marius2401@laposte.net>
2023-01-27 17:05:13 +0100

    Shader maker fix

--- launcher.bat
+++ launcher.bat
@@ -8,7 +8,7 @@ SET LIB_DIR=%ILLOGIC_DIR_MAYA%\lib
 
 rem \---- Shelf
 SET MAYA_SHELF_PATH=%ILLOGIC_DIR_MAYA%\shelfs
-SET XBMLANGPATH=%ILLOGIC_DIR_MAYA%\icons\trashtown_icon;%ILLOGIC_DIR_MAYA%\icons
+SET XBMLANGPATH=%ILLOGIC_DIR_MAYA%\icons\startup_image;%ILLOGIC_DIR_MAYA%\icons
 
 rem \---- Scripts
 SET PYTHONPATH=%PYTHONPATH%;%SCRIPT_MAYA%\shader_maker;%NETWORK_INSTALL%\script;
