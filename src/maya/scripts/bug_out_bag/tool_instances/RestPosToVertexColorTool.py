97526aeb44c5cf5353363ff191acb83ca89dabbe
mariusjenin <marius2401@laposte.net>
2023-05-02 09:59:50 +0200

    Change RestPosToVertexColor

--- src/maya/scripts/bug_out_bag/tool_instances/RestPosToVertexColorTool.py
+++ src/maya/scripts/bug_out_bag/tool_instances/RestPosToVertexColorTool.py
@@ -1,21 +1,69 @@
+import maya.OpenMaya as OpenMaya
 from ..tool_models.ActionTool import *
+from common.utils import *
 
 
 class RestPosToVertexColorTool(ActionTool):
-    @staticmethod
-    def store_rest(obj):
-        rest = "Pref"
-        colorSets = pm.polyColorSet(query=True, allColorSets=True)
+    # @staticmethod
+    # def store_rest_old(obj):
+    #     rest = "Pref"
+    #     colorSets = pm.polyColorSet(query=True, allColorSets=True)
+    #
+    #     if not colorSets or not rest in colorSets:
+    #         pm.polyColorSet(create=True, colorSet=rest, clamped=False, rpt="RGB")
+    #     pm.polyColorSet(obj, currentColorSet=True, colorSet=rest)
+    #     verts = obj.verts
+    #     for v in verts:
+    #         pos = pm.xform(v, q=True, ws=True, t=True)
+    #         pm.select(v)
+    #         pm.polyColorPerVertex(rgb=pos)
+    #     obj.aiExportColors.set(1)
 
-        if not colorSets or not rest in colorSets:
-            pm.polyColorSet(create=True, colorSet=rest, clamped=False, rpt="RGB")
-        pm.polyColorSet(obj, currentColorSet=True, colorSet=rest)
-        verts = obj.verts
-        for v in verts:
-            pos = pm.xform(v, q=True, ws=True, t=True)
-            pm.select(v)
-            pm.polyColorPerVertex(rgb=pos)
-        obj.aiExportColors.set(1)
+    @staticmethod
+    def store_rest(shape):
+        print(shape)
+        color_set_name = "Pref"
+
+        mobj = shape.__apimobject__()
+        dag_path = shape.__apimdagpath__()
+        mesh_fn = OpenMaya.MFnMesh(mobj)
+
+        # Check if the color set exists and create it if it doesn't
+        existing_color_sets = pm.polyColorSet(shape, query=True, allColorSets=True) or []
+
+        if color_set_name not in existing_color_sets:
+            pm.polyColorSet(shape, create=True, colorSet=color_set_name, clamped=False, rpt="RGB")
+
+        # Set "Pref" as the current color set
+        mesh_fn.setCurrentColorSetName(color_set_name)
+
+        vertex_color_list = OpenMaya.MColorArray()
+        mesh_fn.getVertexColors(vertex_color_list)
+        len_vertex_list = vertex_color_list.length()
+
+        fn_component = OpenMaya.MFnSingleIndexedComponent()
+        fn_component.create(OpenMaya.MFn.kMeshVertComponent)
+        fn_component.setCompleteData(len_vertex_list)
+
+        vertex_index_list = OpenMaya.MIntArray()
+        fn_component.getElements(vertex_index_list)
+
+        # Get the vertex positions using the provided code
+        in_mesh_m_point_array = OpenMaya.MPointArray()
+        current_in_mesh_m_fn_mesh = OpenMaya.MFnMesh(dag_path)
+        current_in_mesh_m_fn_mesh.getPoints(in_mesh_m_point_array, OpenMaya.MSpace.kWorld)
+
+        point_list = []
+        for i in range(in_mesh_m_point_array.length()):
+            point_list.append([in_mesh_m_point_array[i][0], in_mesh_m_point_array[i][1], in_mesh_m_point_array[i][2]])
+
+        # Set the vertex colors using the vertex positions
+        for k in range(len_vertex_list):
+            vertex_color_list[k].r = point_list[k][0]
+            vertex_color_list[k].g = point_list[k][1]
+            vertex_color_list[k].b = point_list[k][2]
+
+        mesh_fn.setVertexColors(vertex_color_list, vertex_index_list, None)
 
     def __init__(self):
         super().__init__(name="Rest Pos to Vertex Color", pref_name="rest_pos_to_vertex_color_tool",
@@ -25,6 +73,7 @@ class RestPosToVertexColorTool(ActionTool):
     def _action(self):
         selection = self.__selection
         for elem in selection:
+            # RestPosToVertexColorTool.store_rest(elem)
             RestPosToVertexColorTool.store_rest(elem)
         pm.select(selection)
 
@@ -34,7 +83,11 @@ class RestPosToVertexColorTool(ActionTool):
 
     def __retrieve_selection(self):
         transform = pm.ls(sl=True)
+        shapes = pm.ls(sl=True, shapes=True)
         self.__selection = pm.listRelatives(transform, ad=True, type="mesh")
+        for shape in shapes:
+            if shape not in self.__selection:
+                self.__selection.append(shape)
 
     # Refresh the button on selection changed
     def on_selection_changed(self):
